baadb0fd74d66944f662efa69ada6d02496afa9d
Meowyam <maryam@chilon.net>
2020-06-30 18:50:03 +0800

    import InkyPHAT correctly

--- pascal.py
+++ pascal.py
@@ -1,6 +1,9 @@
-import inkyphat
+from inky import inkyPHAT
 from random import randint
 from PIL import Image, ImageFont, ImageDraw
+
+inky_display = inkyPHAT("red")
+inky_display.set_border(inky_display.WHITE)
 
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
@@ -17,6 +20,9 @@ randQuote = (randint(1, lengthQuotes - 1))
 w, h = font.getsize(message)
 x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
+
+pascal = Image.open("pascal.png")
+inky_display.set_image(pascal)
 
 draw.text((x, y), randQuote, inky_display.BLACK, font)
 inky_display.set_image(img)
