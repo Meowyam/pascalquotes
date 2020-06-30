aae8585b3877bd5d4ed9c42585924c4b5ce58474
Meowyam <maryam@chilon.net>
2020-06-30 20:20:12 +0800

    capitalised inkyphat

--- pascal.py
+++ pascal.py
@@ -9,7 +9,7 @@ inky_display.set_border(inky_display.WHITE)
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
-font = ImageFont.truetype(inkyphat.fonts.AmaticSC, 12)
+font = ImageFont.truetype(InkyPHAT.fonts.AmaticSC, 12)
 
 quotes = open("allQuotes.txt", "r")
 lines = quotes.readlines()
