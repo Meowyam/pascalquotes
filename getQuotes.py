3ba87773c32cd2903f86b8036748e0da2b279c13
Meowyam <maryam@chilon.net>
2020-07-01 12:42:41 +0800

    fixed punctuation for quotes

--- getQuotes.py
+++ getQuotes.py
@@ -8,8 +8,13 @@ tweetList = got.manager.TweetManager.getTweets(tweetCriteria)
 
 quoteFile = open("allQuotes.txt", "a")
 
+wrong_punc = ['’', '‘', '…']
+fixed_punc = ['\'', '\'', '...']
+
 n = len(tweetList)
 for n in tweetList:
+    for idx, punc in enumerate(wrong_punc):
+        n.text = n.text.replace(punc, fixed_punc[idx])
     quoteFile.write(n.text)
     quoteFile.write("\n")
 
